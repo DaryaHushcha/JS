4909666838f6848d735875ab10933a725598a0e5
DaryaHushcha <hushchadaria9002@gmail.com>
2025-09-09 17:24:23 +0200

    Update hush_zewnetrzny.js

--- hush_zewnetrzny.js
+++ hush_zewnetrzny.js
@@ -1,8 +1,8 @@
-document.write("Mój numer w dzienniku to 7"+"<br>");
-document.write("Pierwszy wiersz to:"+"&nbsp;&nbsp;10"+"&nbsp;&nbsp;Tak"+"&nbsp;&nbsp;&nbsp;Nie"+"&nbsp;&nbsp;5"+"<br>");
-document.write("Drugi wiersz to:"+"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;11"+"&nbsp;&nbsp;&nbsp;Tak"+"&nbsp;&nbsp;&nbsp;Tak"+"&nbsp;&nbsp;5"+"<br>");
-document.write("ostatnia modyfikacja strony".fontcolor("#00FF00").bold().fontsize(5)+"<br>"); 
-document.write(document.lastModified.bold().fontcolor("#808000").strike().fontsize(5));
+document.write("Mój numer w dzienniku to 8"+"<br>");
+document.write("Pierwszy wiersz to:"+"&nbsp;&nbsp;9"+"&nbsp;&nbsp;Nie"+"&nbsp;&nbsp;&nbsp;Tak"+"&nbsp;&nbsp;6"+"<br>");
+document.write("Drugi wiersz to:"+"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;9"+"&nbsp;&nbsp;&nbsp;Nie"+"&nbsp;&nbsp;&nbsp;Tak"+"&nbsp;&nbsp;6"+"<br>");
+document.write("ostatnia modyfikacja strony".fontcolor("#008000 ").fontsize(6)+"<br>"); 
+document.write(document.lastModified.fontcolor("#008000 ").strike().fontsize(6));
 
 function okno_zamknij(){ 
                   window.close() } 
